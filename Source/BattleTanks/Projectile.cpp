91d0e22cd243d7df29c9356d794c6a6e48a14785
Pedro <pjsillescas@gmail.com>
2019-03-08 22:27:24 +0100

    Setting up particle systems and projectile.

--- Source/BattleTanks/Projectile.cpp
+++ Source/BattleTanks/Projectile.cpp
@@ -2,12 +2,22 @@
 
 #include "Projectile.h"
 #include "Runtime/Engine/Classes/GameFramework/ProjectileMovementComponent.h"
+#include "Runtime/Engine/Classes/Particles/ParticleSystemComponent.h"
+#include "Runtime/Engine/Classes/Components/StaticMeshComponent.h"
 
 // Sets default values
 AProjectile::AProjectile()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	CollisionMesh = CreateDefaultSubobject<UStaticMeshComponent>(FName("CollisionMesh"));
+	SetRootComponent(CollisionMesh);
+	CollisionMesh->SetNotifyRigidBodyCollision(true);
+	CollisionMesh->SetVisibility(false);
+
+	LaunchBlast = CreateDefaultSubobject<UParticleSystemComponent>(FName("LaunchBlast"));
+	LaunchBlast->AttachTo(RootComponent);
 
 	ProjectileMovementComponent = CreateDefaultSubobject<UProjectileMovementComponent>(FName("ProjectileMovement"));
 	ProjectileMovementComponent->bAutoActivate = false;
--- Source/BattleTanks/Projectile.h
+++ Source/BattleTanks/Projectile.h
@@ -19,8 +19,14 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-	//UPROPERTY(EditDefaultsOnly)
+	UPROPERTY(VisibleAnywhere)
 	class UProjectileMovementComponent* ProjectileMovementComponent;
+
+	UPROPERTY(VisibleAnywhere)
+	class UStaticMeshComponent* CollisionMesh;
+	
+	UPROPERTY(VisibleAnywhere)
+	class UParticleSystemComponent* LaunchBlast;
 
 public:	
 	// Called every frame
